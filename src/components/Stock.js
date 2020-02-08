54e7d1a281088f490bb58b2c682ebbfc13c09b70
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 20:08:52 -0500

    fetch data from apicall

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -19,6 +19,14 @@ class Stock extends Component {
     const API_KEY = process.env.REACT_STOCK_API_KEY;
     let StockSymbol = "FB";
     let API_call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=full&apikey=${API_KEY}`;
+
+    fetch(API_call)
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
   }
 
   render() {
