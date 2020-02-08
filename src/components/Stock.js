cc6c38c3e0525beeeec6d378c813421fcc367923
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 19:52:44 -0500

    stockchart for x n y

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -3,10 +3,20 @@ import React, { Component } from "react";
 class Stock extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: []
+    };
   }
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h1>
+          <strong>Stock Market</strong>
+        </h1>
+      </div>
+    );
   }
 }
 
